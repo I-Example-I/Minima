6768d662c12c4e2f000896b144552a595be37dbf
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-01-11 00:44:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,15 +79,13 @@ CMD="$HOME/minima_service.sh $@"
 /bin/sh -c "$CMD"
 
 echo -e "\033[36m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" 
-echo -e "\033[33m░░░░░░░░Install complete"░░░░░░░░░
+echo -e "\033[33m░░░░░░░░░Install complete░░░░░░░░░"
 echo -e "\033[36m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" 
 #journalctl -fn 10 -u minima_$PORT
 
 #wget -O minima_setup.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_setup.sh && 
 #chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9002 -p 9001
 
-echo -e "\n"
-echo -e "\n"
 echo -e "\n"
 
 echo -e "\033[36m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" 
